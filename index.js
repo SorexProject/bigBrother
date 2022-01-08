8e07fba41bc7c03dfb128e41a6d0466930700820
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-08 23:34:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,36 +4,46 @@ const axios = require("axios");
 require("dotenv").config();
 
 const debug = true;
-
 let aujourdhui = new Date();
+const hostname = process.env.SERVER_PTERODACTYL_URL;
+const tokenUser = process.env.SERVER_PTERODACTYL_TOKENUSER;
+const tokenServer = process.env.SERVER_PTERODACTYL_TOKENSERVER;
 
 server.listen(30001);
 
-app.get('/', function(req, res) {
-    res.send('Ready');
+app.get("/", function(req, res) {
+    res.send("Ready");
 });
 
-const unsplashHeaders = {
-    Authorization: `Client-ID lr1nfucxHIsRAIoIDZqJQTYGIeOjAEFO1jlQ8OoFdOw`,
+const serverHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${tokenUser}`,
 };
 
-const unsplashOptions = {
-    baseURL: "https://api.unsplash.com",
+const serverOptions = {
+    baseURL: hostname,
     port: 443,
     url: "",
     method: "",
-    headers: unsplashHeaders,
+    headers: serverHeaders,
 };
+
+var intervalID = setInterval(alreadyup, 500, "Parameter 1", "Parameter 2");
+
+async function alreadyup() {}
 
 app.get(`/api/image/Random`, async(req, res) => {
     try {
-        unsplashOptions.method = "get";
-        unsplashOptions.url = `/photos/random`;
+        serverOptions.method = "get";
+        serverOptions.url = `/photos/random`;
 
-        const result = await axios(unsplashOptions);
+        const result = await axios(serverOptions);
         if (result) {
             if (debug === true) {
-                console.log(`${aujourdhui} : route ${unsplashOptions.url} ${result.status}`);
+                console.log(
+                    `${aujourdhui} : route ${serverOptions.url} ${result.status}`
+                );
             }
         }
 
@@ -41,7 +51,7 @@ app.get(`/api/image/Random`, async(req, res) => {
             return res.send(result.data);
         }
     } catch (error) {
-        console.log(`${aujourdhui} : route ${unsplashOptions.url} : ${error}`);
+        console.log(`${aujourdhui} : route ${serverOptions.url} : ${error}`);
         return res.send(error);
     }
 });
