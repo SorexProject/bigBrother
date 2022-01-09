964db8bcbd6b71381c71f99b82b12c6766cfcfce
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-09 02:12:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var app = require("express")();
-var server = require("http").Server(app);
-const axios = require("axios");
-require("dotenv").config();
+// import { getResource } from "./element/getResource.ts";
+import express from "express";
+const app = express();
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+import http from "http";
 
 const debug = true;
 let aujourdhui = new Date();
 const hostname = process.env.SERVER_PTERODACTYL_URL;
 const tokenUser = process.env.SERVER_PTERODACTYL_TOKENUSER;
 const tokenServer = process.env.SERVER_PTERODACTYL_TOKENSERVER;
+const port = process.env.SERVER_PORT;
 
-server.listen(process.env.SERVER_PORT);
+http.createServer(app).listen(port);
+console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 
 app.get("/", function(req, res) {
     res.send("Ready");
@@ -31,27 +36,11 @@ const Options = {
     port: 443,
     url: "",
     method: "",
+    headers: null,
 };
 
 var intervalID = setInterval(alreadyup, 10000);
 
-async function getResource(identifier) {
-    try {
-        Options.method = "get";
-        Options.url = `/api/client/servers/${identifier}/resources`;
-        Options.headers = userHeaders;
-        const result = await axios(Options);
-        // debug mode
-        if (result) {
-            if (debug === true) {
-                console.log(`${aujourdhui} : route ${Options.url} ${result.status}`);
-            }
-        }
-    } catch (error) {
-        console.log(`${aujourdhui} : route ${Options.url} : ${error}`);
-        return;
-    }
-}
 async function alreadyup() {
     try {
         let body = [];
@@ -70,7 +59,9 @@ async function alreadyup() {
         if (result) {
             for (let data of result.data.data) {
                 body.push(data.attributes);
-                const diskinit = await getResource(data.attributes.identifier);
+                if (data.attributes.egg == 15) {
+                    const resource = await getResource(data.attributes.identifier);
+                }
             }
         }
     } catch (error) {
@@ -78,3 +69,4 @@ async function alreadyup() {
         return;
     }
 }
+// export { userHeaders, debug };
