00d69e3a138a5c5dbbebb2c9fd6e8cefee3b2d0e
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-09 02:26:51 +0100

    Update getResource.js

--- element/getResource.js
+++ element/getResource.js
@@ -1,7 +1,11 @@
+import { Options, userHeaders, debug } from "../index.js";
+import axios from "axios";
+
+let aujourdhui = new Date();
 async function getResource(identifier) {
     try {
         Options.method = "get";
-        Options.url = `/api/client/servers/${identifier}/resources`;
+        Options.url = `/api/client/servers/${identifier}/players`;
         Options.headers = userHeaders;
         const result = await axios(Options);
         // debug mode
