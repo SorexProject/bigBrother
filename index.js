deba906436d7b6cf867cb3d38faf406d0ef8833b
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-10 22:23:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-import { getPlayers } from "./element/getPlayers.js";
 import { stillThere } from "./element/stillThere.js";
 import express from "express";
 const app = express();
-import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 import http from "http";
-import mysql from "mysql";
-
 
 const debug = true;
 let aujourdhui = new Date();
@@ -15,21 +11,6 @@ const hostname = process.env.SERVER_PTERODACTYL_URL;
 const tokenUser = process.env.SERVER_PTERODACTYL_TOKENUSER;
 const tokenServer = process.env.SERVER_PTERODACTYL_TOKENSERVER;
 const port = process.env.SERVER_PORT;
-
-const mysqld = mysql.createConnection({
-    host: "192.168.1.201",
-    user: "big",
-    password: "P6WOqeBaB8RERIt5m621sE5aR2VuD2",
-    database: "big",
-});
-mysqld.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-
-    console.log("connected as id " + mysqld.threadId);
-});
 
 http.createServer(app).listen(port);
 console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
@@ -56,15 +37,7 @@ const Options = {
     method: "",
     headers: null,
 };
-mysqld.query(
-    "CREATE TABLE IF NOT EXISTS `identifier` (`identifier` TEXT NOT NULL,`time` decimal(30,0) NOT NULL DEFAULT 0) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;",
-    function(error, results, fields) {
-        if (error) throw error;
-        console.log("Database init");
-    }
-);
 
 var intervalID = setInterval(stillThere, 10000);
-
 
 export { Options, userHeaders, debug, serverHeaders };
