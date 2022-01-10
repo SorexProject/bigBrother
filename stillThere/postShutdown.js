be55d80e8006388acf52b68cdf0b856888824ee2
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-11 00:20:37 +0100

    Update postShutdown.js

--- stillThere/postShutdown.js
+++ stillThere/postShutdown.js
@@ -1,20 +1,27 @@
 import { Options, userHeaders, debug } from "../index.js";
 import { mysqld } from "./mysql.js";
+import { body } from "./stillThere.js";
 import axios from "axios";
 
 let aujourdhui = new Date();
 
 async function postShutdown() {
     try {
-        mysqld.query(
-            `SELECT * FROM identifier WHERE identifier="${data.attributes.identifier}"`,
-            function(error, results, fields) {
-                if (error) throw error;
-                for (let res of results) {
 
+        for (let datas of body) {
+            for (let data of datas.data.data) {
+                if (data.attributes.egg == 15 && data.attributes.suspended == false && data.attributes.container.installed == 1) {
+                    mysqld.query(
+                        `SELECT identifier FROM identifier WHERE EXISTS (SELECT identifier FROM identifier WHERE identifier = "${data.attributes.identifier}");`,
+                        function(error, results, fields) {
+                            if (error) throw error;
+                            console.log(results)
+                        }
+                    );
                 }
             }
-        );
+
+        }
 
     } catch (error) {}
 }
