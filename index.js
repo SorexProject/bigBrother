fd4e788ce199d00957b1f0f0365beda4fd6bdb8f
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-01-10 22:16:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { getPlayers } from "./element/getPlayers.js";
+import { stillThere } from "./element/stillThere.js";
 import express from "express";
 const app = express();
 import axios from "axios";
@@ -65,79 +66,5 @@ mysqld.query(
 
 var intervalID = setInterval(stillThere, 10000);
 
-async function stillThere() {
-    try {
-        let body = [];
-        Options.method = "get";
-        Options.url = `/api/application/servers`;
-        Options.headers = serverHeaders;
 
-        const result = await axios(Options);
-        // debug mode
-        if (result) {
-            if (debug === true) {
-                console.log(`${aujourdhui} : route ${Options.url} ${result.status}`);
-            }
-        }
-
-        if (result) {
-            for (let data of result.data.data) {
-                body.push(data.attributes);
-                if (data.attributes.egg == 15 && data.attributes.suspended == false && data.attributes.container.installed == 1) {
-                    const players = await getPlayers(data.attributes.identifier);
-                    if (players.data.onlinePlayers == 0 && players.data.show == 1) {
-                        mysqld.query(
-                            `SELECT identifier FROM identifier WHERE EXISTS (SELECT identifier FROM identifier WHERE identifier = "${data.attributes.identifier}");`,
-                            function(error, results, fields) {
-                                if (error) throw error;
-                                if (results.length == 0) {
-                                    mysqld.query(
-                                        `INSERT INTO big.identifier (identifier) VALUES ("${data.attributes.identifier}");`,
-                                        function(error, results, fields) {
-                                            if (error) throw error;
-                                        }
-                                    );
-                                } else if (players.data.onlinePlayers == 0) {
-                                    mysqld.query(
-                                        `SELECT * FROM identifier WHERE identifier="${data.attributes.identifier}"`,
-                                        function(error, results, fields) {
-                                            if (error) throw error;
-                                            for (let res of results) {
-                                                mysqld.query(
-                                                    `UPDATE big.identifier SET time='${res.time + 1}' WHERE  identifier='${data.attributes.identifier}' AND time=${res.time} LIMIT 1;`,
-                                                    function(error, results, fields) {
-                                                        if (error) throw error;
-                                                    }
-                                                );
-                                            }
-                                        }
-                                    );
-                                }
-                            }
-
-                        );
-                    } else {
-                        mysqld.query(
-                            `SELECT * FROM identifier WHERE identifier="${data.attributes.identifier}"`,
-                            function(error, results, fields) {
-                                if (error) throw error;
-                                for (let res of results) {
-                                    mysqld.query(
-                                        `DELETE FROM big.identifier WHERE identifier='${data.attributes.identifier}' AND time=${res.time} LIMIT 1;`,
-                                        function(error, results, fields) {
-                                            if (error) throw error;
-                                        }
-                                    );
-                                }
-                            }
-                        );
-                    }
-                }
-            }
-        }
-    } catch (error) {
-        console.log(`${aujourdhui} : route ${Options.url} : ${error}`);
-        return;
-    }
-}
-export { Options, userHeaders, debug };
+export { Options, userHeaders, debug, serverHeaders };
