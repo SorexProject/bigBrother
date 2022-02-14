3bc3ec68db3cbdbf0f57734fe05d034bdb841431
Williampilote <57291065+Williampilote@users.noreply.github.com>
2022-02-14 16:27:24 +0100

    Update postShutdown.js

--- service/stillThere/postShutdown.js
+++ service/stillThere/postShutdown.js
@@ -1,17 +1,19 @@
 import { power } from "../../request/pterodactyl/user/power.js";
 import { mysqld } from "../../database/mysql.js";
+import deleteIdentifier from "./deleteIdentifier.js";
 import chalk from "chalk";
 
-const postShutdown = (identifier) => {
+const postShutdown = async(data) => {
     try {
         mysqld.query(
-            `SELECT * FROM identifier WHERE identifier="${identifier}"`,
+            `SELECT * FROM identifier WHERE identifier="${data.attributes.identifier}"`,
             function(error, results, fields) {
                 if (error) throw error;
                 for (let res of results) {
                     if (res.time > 60) {
-                        const players = power(identifier, "kill");
-                        console.log(chalk.yellow(chalk.bold(`${identifier} killed`)))
+                        power(data.attributes.identifier, "kill");
+                        deleteIdentifier(data)
+                        console.log(chalk.yellow(chalk.bold(`${data.attributes.identifier} killed`)))
                         return
                     }
                 }
